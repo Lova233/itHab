59bf081c4d8036c8f0fe3702e6372e31ff08d1c8
lova233 <andrealovati.cube3@gmail.com>
2018-10-31 18:13:38 +0000

    adds route for dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {SignupComponent} from '../app/signup/signup.component'
 import {SigninComponent} from '../app/signin/signin.component'
+import {DashboardComponent} from '../app/containers/dashboard/dashboard.component'
 
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent, },
-  { path: 'signin', component: SigninComponent, }
+  { path: 'signin', component: SigninComponent, },
+  { path: 'dashboard', component: DashboardComponent, }
+
 
   
 ];
