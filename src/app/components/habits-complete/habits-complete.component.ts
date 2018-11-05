12cdb438878326bf3f8df5236f611a56c5868dd0
lova233 <andrealovati.cube3@gmail.com>
2018-11-05 17:21:54 +0000

    sets initial habits complete

--- src/app/components/habits-complete/habits-complete.component.ts
+++ src/app/components/habits-complete/habits-complete.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabitsCompleteComponent implements OnInit {
 
+  habits:any;
+
   constructor() { }
 
   ngOnInit() {
+    this.habits = [
+      { num: "one",   frq:[1,2,3,7]},
+      { num: "two",   frq:[5,6,7]},
+      { num: "three", frq:[2,3,6,5,7]},
+      { num: "four",  frq:[1,2,3,4]},
+      { num: "five",  frq:[1,4,6]}
+    ]
+  }
+  
   }
 
-}
+
