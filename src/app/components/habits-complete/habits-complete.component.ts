13aa997e4d63ff26748063c07732396ba1f80608
lova233 <andrealovati.cube3@gmail.com>
2018-11-12 16:53:18 +0000

    habit logic

--- src/app/components/habits-complete/habits-complete.component.ts
+++ src/app/components/habits-complete/habits-complete.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-habits-complete',
@@ -7,20 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabitsCompleteComponent implements OnInit {
 
+
   habits:any;
+  habit:any
+  habitSelected:any;
+  faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit() {
     this.habits = [
-      { num: "one",   frq:[1,2,3,7]},
-      { num: "two",   frq:[5,6,7]},
-      { num: "three", frq:[2,3,6,5,7]},
-      { num: "four",  frq:[1,2,3,4]},
-      { num: "five",  frq:[1,4,6]}
-    ]
+      { num: "one",   frq:[1,2,3,7], status:""}, 
+      { num: "two",   frq:[5,6,7], status:""},
+      { num: "three", frq:[2,3,6,5,7], status:""},
+      { num: "four",  frq:[1,2,3,4], status:""},
+      { num: "five",  frq:[1,4,6], status:""}
+    ]}
+
+
+  showHabit(habit){
+    this.habitSelected = habit;
+    console.log(habit)
   }
-  
+
+  closeHabit(){
+    this.habitSelected = undefined;
+  }
+  completeHabit(){
+    this.habitSelected.status = true; 
+    console.log( this.habitSelected, "--TRUE DETECTIVE")
   }
 
 
+  }
