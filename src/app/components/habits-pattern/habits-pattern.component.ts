cddbcb4ff8bed6133155b45ec67e97fd52e98ea9
lova233 <andrealovati.cube3@gmail.com>
2018-11-21 14:51:34 +0000

    redesing habit pattern component logic and markup

--- src/app/components/habits-pattern/habits-pattern.component.ts
+++ src/app/components/habits-pattern/habits-pattern.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HabitsPatternComponent implements OnInit {
     
     habits:any;
-
+    monday:Array<any>;
+    tuesday:Array<any>;
+    wednesday:Array<any>;
+    thursday:Array<any>;
+    friday:Array<any>;
+    saturday:Array<any>;
+    sunday:Array<any>;
+    
   constructor() { }
 
   ngOnInit() {
@@ -20,6 +27,18 @@ export class HabitsPatternComponent implements OnInit {
       { num: "four",  frq:[1,2,3,4]},
       { num: "five",  frq:[1,4,6]}
     ]
+
+    //  filte monday = habits when frq = 1
+
+    this.monday = this.habits.filter(habit =>  habit.frq.includes(1));
+    this.tuesday = this.habits.filter(habit =>  habit.frq.includes(2));
+    this.wednesday = this.habits.filter(habit =>  habit.frq.includes(3));
+    this.thursday = this.habits.filter(habit =>  habit.frq.includes(4));
+    this.friday = this.habits.filter(habit =>  habit.frq.includes(5));
+    this.saturday = this.habits.filter(habit =>  habit.frq.includes(6));
+    this.sunday = this.habits.filter(habit =>  habit.frq.includes(7));
+
+    console.log(this.monday, "dave è bravo con il filter")  
   }
   
 
