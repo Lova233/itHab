a19bc113dd051b398d4a29a46596d94ad0265625
lova233 <andrealovati.cube3@gmail.com>
2018-11-28 16:06:28 +0000

    adds habit component

---
+++ src/app/components/habit/habit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-habit',
+  templateUrl: './habit.component.html',
+  styleUrls: ['./habit.component.css']
+})
+export class HabitComponent implements OnInit {
+
+  @Input()
+  habit;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.habit,"dave è bravissimo");
+  }
+
+}
