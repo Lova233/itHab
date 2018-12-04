cb70d3b3081d384f04b899ac54a8bcf74b7b6dfe
lova233 <andrealovati.cube3@gmail.com>
2018-12-04 15:33:45 +0000

    corrects routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {SignupComponent} from '../app/components/signup/signup.component'
 import {SigninComponent} from '../app/components/signin/signin.component'
-import { DashboardComponent } from '../app/containers/dashboard/dashboard.component'
+// import { DashboardComponent } from '../app/containers/dashboard/dashboard.component'
 import { FutureComponent } from './containers/future/future.component';
 import { PastComponent } from './containers/past/past.component';
 import { PresentComponent } from './containers/present/present.component';
