6ff9e332496c8867ba09f4307816936595320d7e
lova233 <andrealovati.cube3@gmail.com>
2018-12-05 13:12:54 +0000

    adds present component markup and backend declaration

--- src/app/containers/present/present.component.ts
+++ src/app/containers/present/present.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HabitService} from '../../services/habit.service';
+
 
 
 
@@ -8,12 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./present.component.css']
 })
 export class PresentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    
-   
-  }
   
+  isPatternOn: boolean;
+  habits:any;
+  isLoaded:boolean;
+
+  constructor(private habitService : HabitService){ }
+
+
+ngOnInit() {
+  this.isLoaded= false;
+  this.habitService.getUserHabits("AndreaLovati").subscribe(
+    habits=> {
+      this.habits=habits.filter(habit=>habit.IsActive)
+      this.habits = this.habits.sort((a,b)=> b.Color.replace("#", "0x")-a.Color.replace("#", "0x"));
+      this.isLoaded=true
+      console.log(this.habits," QUA CI SONO");
+    }
+  )
 }
+
+completeHabit(habitComplete){
+  
+  let payload = {...habitComplete,username:"AndreaLovati",completed_at:null}
+  console.log(payload);
+this.habitService.completed(payload).subscribe(res=>console.log(res));
+}
+
+
+
+}
+
