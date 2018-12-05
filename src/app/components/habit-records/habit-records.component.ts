688da2418180412e1c620338ffa847448872dff4
lova233 <andrealovati.cube3@gmail.com>
2018-12-05 12:45:44 +0000

    creates habits record component

---
+++ src/app/components/habit-records/habit-records.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-records',
+  templateUrl: './habit-records.component.html',
+  styleUrls: ['./habit-records.component.css']
+})
+export class HabitRecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
