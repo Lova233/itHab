1bca4ccaa324e81bca5e4f9dc7957b93166452be
lova233 <andrealovati.cube3@gmail.com>
2018-12-05 13:09:51 +0000

    creates habist complete markup and event emitter

--- src/app/components/habits-complete/habits-complete.component.ts
+++ src/app/components/habits-complete/habits-complete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,29 +7,79 @@ import { faTimes} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./habits-complete.component.css']
 })
 export class HabitsCompleteComponent implements OnInit {
+
+@Output()
+  completedHabit : EventEmitter<any> = new EventEmitter();
+
  @Input()
   habits:any;
 
   habit:any
   habitSelected:any;
   faTimes = faTimes;
+  today:any;
+  parsedToday:string;
+  todaysHabit:any;
 
   constructor() { }
 
   ngOnInit() {
-
+   this.today = new Date().getDay();
+   this.parsedToday = this.parseDay(this.today)
+   this.todaysHabit = this.habits.filter(a => a.Frequency.values.includes(this.today));
   }
-
-
   showHabit(habit){
     this.habitSelected = habit;
+    console.log(this.habitSelected.Color,"la selezionata")
   }
 
-  closeHabit(){
-    this.habitSelected = undefined;
-  }
   completeHabit(){
-    this.habitSelected.status = true; 
+  
+    let habitCompl= {
+      task_id: this.habitSelected.Task_id,
+      completed: true
+    }
+    this.completedHabit.emit(habitCompl);
+  }
+
+  uncompleteHabit(){
+    let habitCompl= {
+      task_id: this.habitSelected.Task_id,
+      completed: false
+    }
+    this.completedHabit.emit(habitCompl);
+  }
+
+  parseDay(x:number){
+    switch (x){
+      case 1:
+      return "Monday";
+      break;
+
+      case 2:
+      return "Tuesday";
+      break;
+
+      case 3:
+      return "Wednesday";
+      break;
+
+      case 4: 
+      return "Thursday";
+      break;
+
+      case 5:
+      return "Friday";
+      break;
+
+      case 6:
+      return "Saturday";
+      break;
+
+      case 7:
+      return "Sunday";
+      break;
+    }
   }
 
 
