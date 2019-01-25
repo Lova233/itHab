4898543d249a56719f9fb45bcc308c1b4b3639c3
lova233 <andrealovati.cube3@gmail.com>
2019-01-25 14:52:44 +0000

    adds habit-complete component

---
+++ src/app/components/habit-complete/habit-complete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-complete',
+  templateUrl: './habit-complete.component.html',
+  styleUrls: ['./habit-complete.component.css']
+})
+export class HabitCompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
