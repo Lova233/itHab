25f36d8e158f10cd713480a1381afd17040a0cdf
lova233 <andrealovati.cube3@gmail.com>
2019-02-21 13:41:32 +0000

    sets fall back route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,16 @@
 
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import {SignupComponent} from '../app/components/signup/signup.component'
-import {SigninComponent} from '../app/components/signin/signin.component'
-// import { DashboardComponent } from '../app/containers/dashboard/dashboard.component'
 import { FutureComponent } from './containers/future/future.component';
 import { PastComponent } from './containers/past/past.component';
 import { PresentComponent } from './containers/present/present.component';
 
 
 const routes: Routes = [
-  { path: 'signup', component: SignupComponent, },
-  { path: 'signin', component: SigninComponent, },
+  { path: '', redirectTo: '/future', pathMatch: 'full' },
   { path: 'future', component: FutureComponent, },
   { path: 'past', component: PastComponent, },
   { path: 'present', component: PresentComponent, }
-
-
-  
 ];
 
 @NgModule({
