c612f3c006116cb72ba0693ca5c8c2aa749538a5
lova233 <andrealovati.cube3@gmail.com>
2019-03-13 00:21:20 +0000

    implement logic for random quotes element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,7 +6,33 @@ import { Component, HostBinding } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ithab';
+export class AppComponent implements OnInit {
+  
+  quotes:object;
+  randomQuote:string;
+  randomCit:string;
+  
+  constructor() { }
+
+
+  ngOnInit(){
+    let quotesIndex = Math.floor((Math.random() * 3) + 1)
+    this.quotes=[
+      {quote:"Infuse your life with action", cit:"Bradley Whitford"},
+      {quote:"The future depends on what you do today", cit:"Mahatma Gandhi"},
+      {quote:"All our dreams can come true, if we have the courage to pursue them", cit:"Walt Disney"},
+      // {quote:"The future depends on what you do today", cit:"Mahatma Gandhi"},
+      // {key:2, quote:"The future depends on what you do today", cit:"Mahatma Gandhi"},
+      // {key:2, quote:"The future depends on what you do today", cit:"Mahatma Gandhi"},
+    ]
+    this.randomQuote = this.quotes[quotesIndex].quote 
+    this.randomCit = this.quotes[quotesIndex].cit
+   
+  }
+
+
+
 
 }
+
+
