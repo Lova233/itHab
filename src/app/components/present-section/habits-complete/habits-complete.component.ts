61f970995dbce42066bade61cc95464a5e39235d
lova233 <andrealovati.cube3@gmail.com>
2019-03-18 13:15:11 +0000

    logic complete

--- src/app/components/present-section/habits-complete/habits-complete.component.ts
+++ src/app/components/present-section/habits-complete/habits-complete.component.ts
@@ -17,24 +17,14 @@ export class HabitsCompleteComponent implements OnInit {
   habitSelected:any;
   faTimes = faTimes;
   today:any;
-  parsedToday:string;
-  currentMonth:any;
   todaysHabit:any;
-  parsedMonth:any;
-  dayNumber:any;
 
   constructor() { }
 
   ngOnInit() {
-   const monthNames = ["January", "February", "March", "April", "May", "June",
-                        "July", "August", "September", "October", "November", "December"];
    this.today = new Date().getDay();
-   this.currentMonth = new Date().getMonth();
-   this.parsedMonth = monthNames[this.currentMonth];
-   this.parsedToday = this.parseDay(this.today);
-   this.dayNumber = new Date().getDate();
    this.todaysHabit = this.habits.filter(a => a.Frequency.values.includes(this.today));
-   console.log(this.parsedToday,this.currentMonth, this.parsedMonth,this.dayNumber,"il giorno di oggi che dovrebbe essere 1")
+  //  console.log(this.parsedToday,this.currentMonth, this.parsedMonth,this.dayNumber,"il giorno di oggi che dovrebbe essere 1")
   }
   showHabit(habit){
     this.habitSelected = habit;
@@ -50,39 +40,4 @@ export class HabitsCompleteComponent implements OnInit {
     }
     this.completedHabit.emit(habitCompl);
   }
-
-
-  parseDay(x:number){
-    switch (x){
-      case 1:
-      return "Monday";
-      break;
-
-      case 2:
-      return "Tuesday";
-      break;
-
-      case 3:
-      return "Wednesday";
-      break;
-
-      case 4: 
-      return "Thursday";
-      break;
-
-      case 5:
-      return "Friday";
-      break;
-
-      case 6:
-      return "Saturday";
-      break;
-
-      case 7:
-      return "Sunday";
-      break;
-    }
-  }
-
-
-  }
+}
